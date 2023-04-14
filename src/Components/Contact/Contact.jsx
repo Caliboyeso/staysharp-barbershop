99248404d47401e07dbc110cf188515d4777f9b5
Jesus Lopez <jesus.blopez97@gmail.com>
2023-04-14 16:46:24 -0700

    Desktop media query for 209 Stay Sharp Barbershop completed.

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -8,6 +8,7 @@ const Contact = () => {
         <div className='Contact-section'>
             <style>@importurl('https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap');</style>
             <h1 className='Contact-header'>Contact</h1>
+            <div className='Contact-main-div'>
             <a
                 id="Contact-map-link"
                 href="https://www.google.com/maps/place/209+Stay+Sharp+Barbershop/@37.797122,-121.2419772,17z/data=!3m1!4b1!4m5!3m4!1s0x8090411507f96bad:0x82b73283294af2af!8m2!3d37.7970395!4d-121.2397858"
@@ -43,6 +44,7 @@ const Contact = () => {
                         <li>Sun: 9AM - 4PM</li>
                     </ul>
                 </div>
+            </div>
             </div>
         </div>
     );
